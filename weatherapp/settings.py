589f1bf7a10bbf92bcd5b8aae052bf9202051b26
Laura Brusco <lorabrus35@gmail.com>
2026-01-13 13:50:41 +0100

    Remove .env and database from repository history

--- weatherapp/settings.py
+++ weatherapp/settings.py
@@ -25,7 +25,7 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(rv_b^ffr3y0^edp!%2(na5d^74r+33(ezzg1re)k9f^i8i+7c'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
